05096a9301f06b48359919637a0bc32f485c9963
mashuangfei <mashuangfei@meituan.com>
2022-12-15 16:36:15 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "me.haku"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation("org.slf4j", "slf4j-api", "1.7.26")
+    implementation("org.slf4j", "slf4j-simple", "1.7.26")
+    implementation("org.jetbrains.kotlinx", "kotlin-deeplearning-api", "0.2.0")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
